66061743f89aae87ab8462ef542f17ce35545523
Fernando Barraza <fernandobarraza@192.168.1.23>
2023-05-21 21:01:50 -0700

    GSAP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {gsap} from 'gsap'
+
+gsap.to('.home', {
+    x: 400,
+    duration: 3,
+    markers: true,
+    start: "top top",
+    end: "bottom bottom"
+})
+</script>
 
 <template>
-    <main class="bg-purple-heart h-screen">Home</main>
+    <main ref="homeElement" class="home bg-purple-heart h-screen">Home</main>
 </template>
