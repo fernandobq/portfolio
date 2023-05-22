419ec13e1e9916184ce661eac2054a843655c999
Fernando Barraza <fernandobarraza@192.168.1.23>
2023-05-21 22:15:48 -0700

    Added gsap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
-import {gsap} from 'gsap'
+import { gsap } from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import { onMounted, ref } from 'vue'
+gsap.registerPlugin(ScrollTrigger)
+const square = ref()
+const homeElement = ref()
 
-gsap.to('.home', {
-    x: 400,
-    duration: 3,
-    markers: true,
-    start: "top top",
-    end: "bottom bottom"
+
+onMounted(() => {
+    gsap.to(square.value, {
+        x: 700,
+        scrollTrigger: {
+            trigger: square.value,
+            start: "top 600",
+            end: "top 400",
+            markers: true,
+            scrub: 1,
+        }
+    })
 })
 </script>
 
 <template>
-    <main ref="homeElement" class="home bg-purple-heart h-screen">Home</main>
+    <main class="trigger h-screen">
+        <div ref="trigger" class="square w-20 h-20 bg-green">Cuadro2</div>
+    </main>
+    <main ref="homeElement" class="home bg-purple-heart h-screen">
+        <div ref="square" class="square w-20 h-20 bg-green mt-auto">Cuadro</div>
+    </main>
 </template>
