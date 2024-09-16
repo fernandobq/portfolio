de98b7ad79f68d81439ebd1eee97f1c90e7c131b
Fernando Barraza Quintero <febaqui1008@gmail.com>
2024-09-16 14:47:31 -0700

    Add the selected project modal

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import Modal from '@/components/Modal.vue'
 import SingleProject from './SingleProject.vue'
 import projects from '../assets/data/projects.json'
+import type { ProjectPropsT } from '@/components/SingleProject.vue'
+import ProjectModalContent from '@/components/ProjectModalContent.vue'
+
+const openModal = ref<boolean>(false)
+const selectedProject = ref<ProjectPropsT>()
+
+const handleOpenModal = (technology: ProjectPropsT) => {
+    selectedProject.value = technology
+    openModal.value = true
+}
 </script>
 
 <template>
@@ -15,8 +27,18 @@ import projects from '../assets/data/projects.json'
                     :project="project"
                     :key="project.id"
                     class="lg:col-span-2"
+                    @select-project="(val) => handleOpenModal(val)"
                 />
             </div>
         </div>
+        <Modal
+            :open-modal="openModal"
+            modal-classes="max-h-[60%] overflow-y-auto top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-mobile-modal bg-haiti text-white rounded-xl px-8 py-10 md:w-[500px] md:max-h-[70%] lg:w-[650px] lg:px-12 lg:py-14 xl:max-h-[80%] xl:px-14 xl:py-16"
+            @closeModal="() => (openModal = false)"
+        >
+            <div v-if="selectedProject">
+                <ProjectModalContent :selected-project="selectedProject" />
+            </div>
+        </Modal>
     </section>
 </template>
